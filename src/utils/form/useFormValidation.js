1597670923061083a18aa32002030175bca04bd3
alekcarvalho <alek_carvalho@hotmail.com>
2022-04-04 10:21:11 -0400

    add form validators

---
+++ src/utils/form/useFormValidation.js
@@ -0,0 +1,57 @@
+import { reactive } from '@vue/reactivity'
+import useValidators from '~/utils/form/validators'
+
+const errors = reactive({})
+errors.form = []
+
+const removeElement = (item) => {
+  const arr = errors.form
+  const index = errors.form.indexOf(item)
+  if (index > -1) {
+    return arr.splice(index, 1)
+  }
+}
+
+export default function useFormValidation() {
+  const { isEmpty, minLength, isEmail, isNum } = useValidators()
+
+  const validateNameField = (fieldName, fieldValue) => {
+    errors[fieldName] = !fieldValue
+      ? isEmpty(fieldName, fieldValue)
+      : minLength(fieldName, fieldValue, 4)
+    errors[fieldName] ? errors.form.push(fieldName) : removeElement(fieldName)
+    errors.form = [...new Set(errors.form)]
+  }
+
+  const validateEmailField = (fieldName, fieldValue) => {
+    errors[fieldName] = !fieldValue
+      ? isEmpty(fieldName, fieldValue)
+      : isEmail(fieldName, fieldValue)
+    errors[fieldName] ? errors.form.push(fieldName) : removeElement(fieldName)
+    errors.form = [...new Set(errors.form)]
+  }
+
+  const validatePhoneField = (fieldName, fieldValue) => {
+    errors[fieldName] = !fieldValue
+      ? isEmpty(fieldName, fieldValue)
+      : isNum(fieldName, fieldValue)
+    errors[fieldName] ? errors.form.push(fieldName) : removeElement(fieldName)
+    errors.form = [...new Set(errors.form)]
+  }
+
+  const validatePasswordField = (fieldName, fieldValue) => {
+    errors[fieldName] = !fieldValue
+      ? isEmpty(fieldName, fieldValue)
+      : minLength(fieldName, fieldValue, 8)
+    errors[fieldName] ? errors.form.push(fieldName) : removeElement(fieldName)
+    errors.form = [...new Set(errors.form)]
+  }
+
+  return {
+    errors,
+    validateNameField,
+    validateEmailField,
+    validatePhoneField,
+    validatePasswordField,
+  }
+}
